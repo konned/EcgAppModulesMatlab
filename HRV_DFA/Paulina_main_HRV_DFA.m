873ea470f8377c5299344e69cb7bafaf5fa41f22
Behorsed <paulina.e.kowalik@wp.pl>
2018-12-03 19:14:22 +0100

    modul przetestowany na outpucie modulu R_peaks

--- HRV_DFA/Paulina_main_HRV_DFA.m
+++ HRV_DFA/Paulina_main_HRV_DFA.m
@@ -1,7 +1,9 @@
-clear all;
+% README
+% W³¹czyæ najpierw main ECG_BASELINE
+% Potem main R_PEAKS_WAVES
+% Potem dopiero ten plik
+% Albowiem output Rpeaks modu³u R_PEAKS_WAVES jest
+% inputem modu³u HRV_DFA
 close all;
-hrv = load('nsr001.dat');
-hrv=hrv';% to tylko przyklad, prawdziwy sygnal 
-% to nie moze byc tachogram, tylko numery probek pikow R
 
-[alpha1, alpha2] = HRV_DFA(hrv);
+[alpha1, alpha2] = HRV_DFA(Rpeaks);
